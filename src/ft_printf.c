8e235da594972b3aaf82204e8d15ab74e4f7a255
seungwoo7050 <seungwoo7050@naver.com>
2025-01-29 13:38:05 +0900

    refactor(core): 리터럴 출력을 컨텍스트 API로 이관

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,51 +1,34 @@
-#include "ft_printf.h"
+#include "ft_printf_internal.h"
 
-#include <limits.h>
 #include <stdarg.h>
-#include <unistd.h>
-
-static int	ft_write_count(const char *buffer, int length, int *count)
-{
-	int	written;
-
-	if (length <= 0)
-		return (0);
-	if (*count > INT_MAX - length)
-		return (-1);
-	written = (int)write(1, buffer, (size_t)length);
-	if (written < 0 || written != length)
-		return (-1);
-	*count += written;
-	return (0);
-}
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
-	int		count;
+	t_printf	ctx;
 
 	(void)args;
 	if (format == 0)
 		return (-1);
 	va_start(args, format);
-	count = 0;
+	ft_printf_init(&ctx, 1);
 	while (*format)
 	{
 		if (*format == '%' && *(format + 1) == '%')
 		{
-			if (ft_write_count("%", 1, &count) < 0)
-				count = -1;
+			if (ft_printf_putchar(&ctx, '%') < 0)
+				break ;
 			format += 2;
 		}
 		else
 		{
-			if (ft_write_count(format, 1, &count) < 0)
-				count = -1;
+			if (ft_printf_putchar(&ctx, *format) < 0)
+				break ;
 			format++;
 		}
-		if (count < 0)
-			break ;
 	}
 	va_end(args);
-	return (count);
+	if (ctx.error)
+		return (-1);
+	return (ctx.count);
 }
