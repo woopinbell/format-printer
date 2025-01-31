a6e99def7239808e4bd1310bd3c4ac7f2d4c74c9
seungwoo7050 <seungwoo7050@naver.com>
2025-01-31 12:43:56 +0900

    feat(hex): 16진수와 포인터 너비와 정렬 적용

--- src/ft_hex.c
+++ src/ft_hex.c
@@ -2,40 +2,65 @@
 
 #include <stdint.h>
 
-static int	ft_print_base(t_printf *ctx, unsigned long number, const char *digits)
+static int	ft_hex_digits(char *buffer, unsigned long number, const char *base)
 {
-	char	buffer[2 + sizeof(unsigned long) * 2];
+	char	reversed[2 + sizeof(unsigned long) * 2];
+	int		length;
 	int		index;
 
-	index = 0;
+	length = 0;
 	if (number == 0)
-		buffer[index++] = '0';
+		reversed[length++] = '0';
 	while (number > 0)
 	{
-		buffer[index++] = digits[number % 16];
+		reversed[length++] = base[number % 16];
 		number /= 16;
 	}
-	while (index > 0)
+	index = 0;
+	while (index < length)
 	{
-		index--;
-		if (ft_printf_putchar(ctx, buffer[index]) < 0)
-			return (-1);
+		buffer[index] = reversed[length - 1 - index];
+		index++;
 	}
+	return (length);
+}
+
+static int	ft_write_hex(t_printf *ctx, t_format *fmt, const char *prefix,
+		unsigned long number)
+{
+	char	digits[2 + sizeof(unsigned long) * 2];
+	int		digit_len;
+	int		prefix_len;
+	int		padding;
+
+	if (fmt->spec == 'X')
+		digit_len = ft_hex_digits(digits, number, "0123456789ABCDEF");
+	else
+		digit_len = ft_hex_digits(digits, number, "0123456789abcdef");
+	prefix_len = 0;
+	while (prefix[prefix_len])
+		prefix_len++;
+	padding = fmt->width - prefix_len - digit_len;
+	if (!(fmt->flags & FT_FLAG_LEFT)
+		&& ft_printf_putnchar(ctx, ' ', padding) < 0)
+		return (-1);
+	if (ft_printf_write(ctx, prefix, (size_t)prefix_len) < 0)
+		return (-1);
+	if (ft_printf_write(ctx, digits, (size_t)digit_len) < 0)
+		return (-1);
+	if ((fmt->flags & FT_FLAG_LEFT)
+		&& ft_printf_putnchar(ctx, ' ', padding) < 0)
+		return (-1);
 	return (0);
 }
 
 int	ft_printf_print_hex(t_printf *ctx, t_format *fmt, unsigned int number)
 {
-	if (fmt->spec == 'X')
-		return (ft_print_base(ctx, (unsigned long)number, "0123456789ABCDEF"));
-	return (ft_print_base(ctx, (unsigned long)number, "0123456789abcdef"));
+	return (ft_write_hex(ctx, fmt, "", (unsigned long)number));
 }
 
 int	ft_printf_print_pointer(t_printf *ctx, t_format *fmt, void *pointer)
 {
-	(void)fmt;
-	if (ft_printf_write(ctx, "0x", 2) < 0)
-		return (-1);
-	return (ft_print_base(ctx, (unsigned long)(uintptr_t)pointer,
-			"0123456789abcdef"));
+	return (ft_write_hex(ctx, fmt, "0x",
+			(unsigned long)(uintptr_t)pointer));
 }
