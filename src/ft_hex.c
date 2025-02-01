c09cd19c287e9adfc9b912f516bf027cfc80b700
seungwoo7050 <seungwoo7050@naver.com>
2025-02-01 09:06:33 +0900

    feat(numeric): 숫자 정밀도와 0 채움 적용

--- src/ft_hex.c
+++ src/ft_hex.c
@@ -31,20 +31,38 @@ static int	ft_write_hex(t_printf *ctx, t_format *fmt, const char *prefix,
 	char	digits[2 + sizeof(unsigned long) * 2];
 	int		digit_len;
 	int		prefix_len;
+	int		zero_len;
 	int		padding;
+	char	pad_char;
 
 	if (fmt->spec == 'X')
 		digit_len = ft_hex_digits(digits, number, "0123456789ABCDEF");
 	else
 		digit_len = ft_hex_digits(digits, number, "0123456789abcdef");
+	if (fmt->has_precision && fmt->precision == 0 && number == 0)
+		digit_len = 0;
 	prefix_len = 0;
 	while (prefix[prefix_len])
 		prefix_len++;
-	padding = fmt->width - prefix_len - digit_len;
+	zero_len = 0;
+	if (fmt->has_precision && fmt->precision > digit_len)
+		zero_len = fmt->precision - digit_len;
+	padding = fmt->width - prefix_len - zero_len - digit_len;
+	pad_char = ' ';
+	if ((fmt->flags & FT_FLAG_ZERO) && !(fmt->flags & FT_FLAG_LEFT)
+		&& !fmt->has_precision)
+		pad_char = '0';
 	if (!(fmt->flags & FT_FLAG_LEFT)
+		&& pad_char == ' '
 		&& ft_printf_putnchar(ctx, ' ', padding) < 0)
 		return (-1);
 	if (ft_printf_write(ctx, prefix, (size_t)prefix_len) < 0)
+		return (-1);
+	if (!(fmt->flags & FT_FLAG_LEFT)
+		&& pad_char == '0'
+		&& ft_printf_putnchar(ctx, '0', padding) < 0)
+		return (-1);
+	if (ft_printf_putnchar(ctx, '0', zero_len) < 0)
 		return (-1);
 	if (ft_printf_write(ctx, digits, (size_t)digit_len) < 0)
 		return (-1);
--- src/ft_number.c
+++ src/ft_number.c
@@ -4,6 +4,7 @@ static int	ft_decimal_digits(char *buffer, unsigned long number)
 {
 	char	reversed[20];
 	int		length;
+	int		index;
 
 	length = 0;
 	if (number == 0)
@@ -13,11 +14,11 @@ static int	ft_decimal_digits(char *buffer, unsigned long number)
 		reversed[length++] = (char)('0' + number % 10);
 		number /= 10;
 	}
-	number = 0;
-	while (number < (unsigned long)length)
+	index = 0;
+	while (index < length)
 	{
-		buffer[number] = reversed[length - 1 - number];
-		number++;
+		buffer[index] = reversed[length - 1 - index];
+		index++;
 	}
 	return (length);
 }
@@ -28,17 +29,35 @@ static int	ft_write_decimal(t_printf *ctx, t_format *fmt,
 	char	digits[20];
 	int		digit_len;
 	int		prefix_len;
+	int		zero_len;
 	int		padding;
+	char	pad_char;
 
 	digit_len = ft_decimal_digits(digits, number);
+	if (fmt->has_precision && fmt->precision == 0 && number == 0)
+		digit_len = 0;
 	prefix_len = 0;
 	while (prefix[prefix_len])
 		prefix_len++;
-	padding = fmt->width - prefix_len - digit_len;
+	zero_len = 0;
+	if (fmt->has_precision && fmt->precision > digit_len)
+		zero_len = fmt->precision - digit_len;
+	padding = fmt->width - prefix_len - zero_len - digit_len;
+	pad_char = ' ';
+	if ((fmt->flags & FT_FLAG_ZERO) && !(fmt->flags & FT_FLAG_LEFT)
+		&& !fmt->has_precision)
+		pad_char = '0';
 	if (!(fmt->flags & FT_FLAG_LEFT)
+		&& pad_char == ' '
 		&& ft_printf_putnchar(ctx, ' ', padding) < 0)
 		return (-1);
 	if (ft_printf_write(ctx, prefix, (size_t)prefix_len) < 0)
+		return (-1);
+	if (!(fmt->flags & FT_FLAG_LEFT)
+		&& pad_char == '0'
+		&& ft_printf_putnchar(ctx, '0', padding) < 0)
+		return (-1);
+	if (ft_printf_putnchar(ctx, '0', zero_len) < 0)
 		return (-1);
 	if (ft_printf_write(ctx, digits, (size_t)digit_len) < 0)
 		return (-1);
